18f1f4cf95a78295e2a043da360ddfbe3b024efb
Denis Dziganchuk <73368785+DenisDziganchuk@users.noreply.github.com>
2023-01-25 02:43:41 +0000

    Added main file

---
+++ main.py
@@ -0,0 +1,18 @@
+from io import BytesIO
+from flask import Flask, request, Response
+from qrcode import make
+from PIL import Image
+
+app = Flask(__name__)
+
+@app.route('/qr', methods=['GET'])
+def create_qr():
+    url = request.args.get('url')
+    qr = make(url)
+    img_io = BytesIO()
+    qr.save(img_io, 'PNG')
+    img_io.seek(0)
+    return Response(img_io.read(),content_type="image/png",headers={"Content-Disposition":"attachment;filename=qr.png"})
+
+if __name__ == '__main__':
+    app.run(debug=True)
